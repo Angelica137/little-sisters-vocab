72f71b56fd40265d4cf77321f332e809fd0cd96a
Angelica <Angelica137@users.noreply.github.com>
2022-02-05 21:53:34 +0000

    .make_word_groups() using en prefix, green

--- scripts/strings.py
+++ scripts/strings.py
@@ -3,3 +3,12 @@ def add_prefix_un(word: str) -> str:
     This function takes `word` as a paramater and returns a new word with an 'un' prefix.
     """
     return f'un{word}'
+    # O(n^2)
+
+
+def make_word_groups(vocab_words: list) -> str:
+    """
+    This function takes a `vocab_words` list and returns a strong with the prefix and the words with the prefix applied, separated by '::'.
+    """
+    prefix = " :: " + vocab_words[0]
+    return prefix.join(vocab_words)
