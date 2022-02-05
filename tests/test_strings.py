4ebe6ce15564bf0637810251fd57d656e6890931
Angelica <Angelica137@users.noreply.github.com>
2022-02-05 21:17:18 +0000

    .add_prefix_un() returns unmanegeable, green

--- tests/test_strings.py
+++ tests/test_strings.py
@@ -3,3 +3,7 @@ from scripts.strings import *
 
 def test_add_prefix_un_returns_unhappy():
     assert add_prefix_un("happy") == "unhappy"
+
+
+def test_add_prefix_un_returns_unmanageable():
+    assert add_prefix_un("manageable")
