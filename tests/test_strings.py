701433885d040f0bc9cc9e61102bc5d91e3248f2
Angelica <Angelica137@users.noreply.github.com>
2022-02-06 13:12:37 +0000

    .make_word_groups() using pre  prefix, green

--- tests/test_strings.py
+++ tests/test_strings.py
@@ -12,3 +12,8 @@ def test_add_prefix_un_returns_unmanageable():
 def test_make_word_groups_en():
     assert make_word_groups(
         ['en', 'close', 'joy', 'lighten']) == 'en :: enclose :: enjoy :: enlighten'
+
+
+def test_make_word_groups_pre():
+    assert make_word_groups(['pre', 'serve', 'dispose', 'position']
+                            ) == 'pre :: preserve :: predispose :: preposition'
