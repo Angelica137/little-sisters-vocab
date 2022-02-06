ed24038cc8935dafa493287db8fbb6b705629c89
Angelica <Angelica137@users.noreply.github.com>
2022-02-06 19:01:30 +0000

    refactor

--- scripts/strings.py
+++ scripts/strings.py
@@ -12,7 +12,7 @@ def make_word_groups(vocab_words: list) -> str:
     """
     This function takes a `vocab_words` list and returns a strong with the prefix and the words with the prefix applied, separated by '::'.
     """
-    prefix = " :: " + vocab_words[0]  # O(n^2)
+    prefix = f' :: {vocab_words[0]}'  # O(n^2)
     return prefix.join(vocab_words)  # O(n)
 
 
@@ -21,7 +21,7 @@ def remove_suffix_ness(word: str) -> str:
     This function takes in a word and returns the base word with `ness` removed
     """
     if word[-5:] == 'iness':
-        return word[0:-5] + 'y'
+        return f'{word[0:-5]}y'
     return word[0:-4]
 
 
@@ -30,6 +30,6 @@ def adjective_to_verb(sentence: str, index: int) -> str:
     This function takes a `sentence` using the vocablulary word and the `index` of the word.
     The function returns the extracted adjective as a verb.
     """
-    words = sentence.translate(str.maketrans('', '', string.punctuation))
-    words = words.split()
+    words = sentence.translate(str.maketrans(
+        '', '', string.punctuation)).split()
     return words[index] + 'en'
