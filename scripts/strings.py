3c35eebbebb65b3c17661c7194f40a0d63725dc6
Angelica <Angelica137@users.noreply.github.com>
2022-02-09 20:57:46 +0000

    refactor using the ternmiary operator, red

--- scripts/strings.py
+++ scripts/strings.py
@@ -19,9 +19,8 @@ def remove_suffix_ness(word: str) -> str:
     """
     This function takes in a word and returns the base word with `ness` removed
     """
-    if word[-5:] == 'iness':
-        return word[0:-5] + 'y'
-    return word[0:-4]
+    ending = word[-5:]
+    return (word[0:-5] + 'y') if (ending := 'iness') else word[0:-4]
 
 
 def adjective_to_verb(sentence: str, index: int) -> str:
